1eba82c1cb6ce025f9b01473cf3aa648aa4a8758
Kelsi Summers <kelsisummers@Kelsis-MacBook-Pro.local>
2020-08-24 16:33:34 -0600

    start of line animation

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,35 +2,24 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import styles from './Hero.module.scss';
 
-const Hero = () => {
-  window.addEventListener('DOMContentLoaded', () => {
-
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        const id = entry.target.getAttribute('id');
-        if (entry.intersectionRatio > 0) {
-          document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-        } else {
-          document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
-        }
-      });
-    });
-  
-    // Track all sections that have an `id` applied
-    document.querySelectorAll('section[id]').forEach((section) => {
-      observer.observe(section);
-    });
-    
-  });
-  return (
+const Hero = () => (
   <div className={styles.Hero}>
-    <h1>alt creative</h1>
-    <h2>creative work to empower the collective.</h2>
+    <div className={styles.Hero__animation}>
+      <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 100.6 107.6" id="star-svg" className={styles.Hero__svg} >
+          <path vectorEffect="non-scaling-stroke" id='star-path' className={styles.Hero__path} fill="none" strokeWidth="3" stroke="#000" 
+          d="M90,15v-14H2v21h100" />
+      </svg>
+    </div>
+    <div className={styles.Hero__copy}>
+      <h1>alt creative</h1>
+      <h2>creative work to empower the collective.</h2>
+    </div>
+
   </div>
 );
-  }
-// Hero.propTypes = {};
 
-// Hero.defaultProps = {};
+Hero.propTypes = {};
+
+Hero.defaultProps = {};
 
 export default Hero;
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -28,3 +28,60 @@ const Services = () => (
 // Services.defaultProps = {};
 
 export default Services;
+
+
+// Line animation starter code
+
+// CSS
+// const animation = {
+//   position: "fixed",
+//   top: "250px",
+//   left: "0",
+//   width: "100%",
+//   height: "100px",
+//   margin: "-75px 0 0 -75px",
+//   zIndex: "-1"
+// }
+
+// Javascript
+//   componentDidMount() {
+//     // Get a reference to the <path>
+//     let path = document.querySelector('#star-path');
+
+//     // Get length of path... ~577px in this case
+//     let pathLength = path.getTotalLength();
+
+//     // Make very long dashes (the length of the path itself)
+//     path.style.strokeDasharray = pathLength + ' ' + pathLength;
+//     // Offset the dashes so the it appears hidden entirely
+//     path.style.strokeDashoffset = pathLength;
+
+//     // Jake Archibald says so
+//     // https://jakearchibald.com/2013/animated-line-drawing-svg/
+//     path.getBoundingClientRect()
+
+//     // When the page scrolls...
+//     window.addEventListener("scroll", function(e) {
+    
+//       // What % down is it? 
+//       // https://stackoverflow.com/questions/2387136/cross-browser-method-to-determine-vertical-scroll-percentage-in-javascript/2387222#2387222
+//       // Had to try three or four differnet methods here. Kind of a cross-browser nightmare.
+//       let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+        
+//       // Length to offset the dashes
+//       let drawLength = pathLength * scrollPercentage;
+      
+//       // Draw in reverse
+//       path.style.strokeDashoffset = pathLength - drawLength;
+        
+//       // When complete, remove the dash array, otherwise shape isn't quite sharp
+//       // Accounts for fuzzy math
+//       if (scrollPercentage >= 0.99) {
+//         path.style.strokeDasharray = "none";
+        
+//       } else {
+//         path.style.strokeDasharray = pathLength + ' ' + pathLength;
+//       }
+      
+//     });
+//   }
